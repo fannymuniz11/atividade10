1e3330d049048f79bb1d46281ed3d6b277261cdf
fannymuniz11 <munizfanny03@gmail.com>
2024-09-11 14:16:19 -0300

    atividade10

---
+++ atividade10.py
@@ -0,0 +1,16 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+Numb = int(input("Seu inteiro aqui"))
+
+if Numb % 2 ==0:
+ print("Seu numero é par!")   
+
+else:
+    print("seu numero é impar!")
+
+
+
+
+
+
+
